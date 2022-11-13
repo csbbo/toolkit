e69c5f8d802682bf621329e98d29d789dddf549d
chen <chshbox@gmail.com>
2022-11-13 18:45:50 +0800

    feat: 添加group

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,28 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from account.models import Group, User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "username",
+        "name",
+        "phone",
+        "email",
+        "is_staff",
+        "is_active",
+        "is_superuser",
+        "first_name",
+        "last_name",
+        "date_joined",
+        "last_login",
+        "create_time",
+        "update_time",
+    )
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ("name",)
--- account/models.py
+++ account/models.py
@@ -1,26 +1,25 @@
-from django.contrib.auth.base_user import AbstractBaseUser
-from django.contrib.auth.models import UserManager
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import Group as AbstractGroup
 from django.db import models
 
 from common.models import CUBaseModel
 
 
-# password与last_login是AbstractBaseUser中字段
-class User(AbstractBaseUser, CUBaseModel):
-    id = models.BigAutoField(primary_key=True, auto_created=True)
-    username = models.CharField(max_length=120, unique=True, db_index=True)
-    phone = models.CharField(max_length=11, null=True, unique=True, db_index=True)
-    email = models.CharField(max_length=120, null=True, unique=True, db_index=True)
-    name = models.CharField(max_length=120, null=True)
-    password = models.CharField(max_length=128, null=True)
-    is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
-    signature = models.CharField(max_length=120, default="")
-    last_login_time = models.DateTimeField(null=True)
+class User(AbstractUser, CUBaseModel):
+    """
+    User全部字段查看AbstractUser, username, email, password等字段使用django默认支持的
+    """
 
-    USERNAME_FIELD = "username"
-    objects = UserManager()
+    id = models.BigAutoField(primary_key=True, auto_created=True)
+    phone = models.CharField(max_length=11, null=True, unique=True, db_index=True)
+    name = models.CharField(max_length=120, null=True)
+    signature = models.CharField(max_length=120, default="")
+    update_time = models.DateTimeField(auto_now=True)
 
     class Meta:
-        db_table = "account_user"
-        ordering = ("-create_time",)
+        ordering = ("-update_time",)
+
+
+class Group(AbstractGroup, CUBaseModel):
+    class Meta:
+        ordering = ("-update_time",)
