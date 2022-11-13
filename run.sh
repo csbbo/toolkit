741659817b02d5de6aa83980ce30c9e2ccfef03d
chen <chshbox@gmail.com>
2022-11-13 23:04:32 +0800

    feat: collection static

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+echo -e "\033[31m - collection static \033[0m"
+python manage.py collectstatic
+
 echo -e "\033[31m - run databases migrate \033[0m"
 while :; do
     if python3 manage.py migrate; then
