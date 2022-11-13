5a7d0d260a00a931820481b59c7d768c05397413
chen <chshbox@gmail.com>
2022-11-13 22:09:41 +0800

    fix: openresty build

--- quick_deploy.sh
+++ quick_deploy.sh
@@ -1,4 +1,4 @@
 docker-compose -f docker-compose.yml down
+docker build -t tk_openresty ./openresty
 docker build -t tk_server .
-docker build -t tk_nginx nginx
 docker-compose -f docker-compose.yml up -d
