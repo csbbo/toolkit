8cb5e347e6b8f96072f457a26f0fabd64cee60ab
chen <chshbox@gmail.com>
2022-11-13 23:38:01 +0800

    feat: collectstatic

--- toolkit/settings.py
+++ toolkit/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = "/openresty/collectstatic"
+STATIC_ROOT = "/collectstatic"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
