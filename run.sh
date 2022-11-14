58e37979609ce92156b4266c141ad015424dc88c
chen <chshbox@gmail.com>
2022-11-14 20:36:47 +0800

    feat: collectstatic no input

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 echo -e "\033[31m - collection static \033[0m"
-python3 manage.py collectstatic
+python3 manage.py collectstatic --noinput
 
 echo -e "\033[31m - run databases migrate \033[0m"
 while :; do
