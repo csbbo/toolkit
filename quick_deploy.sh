caf57f1b46f2c91b524d9884c87a994e6ce25df3
chen <chshbox@gmail.com>
2022-11-14 21:11:19 +0800

    feat: update tool

--- quick_deploy.sh
+++ quick_deploy.sh
@@ -1,4 +1,4 @@
 docker-compose -f docker-compose.yml down
-docker build -t tk_openresty ./openresty
-docker build -t tk_server .
+docker build -t tool_openresty ./openresty
+docker build -t tool_server .
 docker-compose -f docker-compose.yml up -d
